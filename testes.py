365594173efe18e21c44ab6daa66e0361772324e
Ricardo <kardopoly@gmail.com>
2020-08-31 17:30:12 -0300

    criando uma nova classificaçao e decodificando o json para utf-8

--- testes.py
+++ testes.py
@@ -2,7 +2,7 @@ import os, json
 
 from bottle import run, get
 
-with open('classificacao.json') as f:
+with open('classificacao.json',  encoding='utf8') as f:
   titulos = json.load(f)
 
 @get('/lista')
@@ -12,13 +12,13 @@ def getAll():
 @get('/titulo/<id>')
 def pegaUm(id):
     tit='sem'
-    print('id:', id)
+    # print('id:', id)
     for t in titulos:
-        print(t['ids'])
+        # print(t['ids'])
         for x in t['ids']:
-            print('titulo:',t['titulo'])
             if int(x)==int(id):
                 tit = t['titulo']
+                print('titulo:', t['titulo'], 'id:', x)
     return tit
 
 if __name__ == '__main__':
